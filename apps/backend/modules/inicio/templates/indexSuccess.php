0cf241e017a5c4d290fb1b18ea173ffdc051e961
Harold <m3ll0x77@gmail.com>
2011-11-19 18:56:35 -0300

    agregado el login

---
+++ apps/backend/modules/inicio/templates/indexSuccess.php
@@ -0,0 +1,53 @@
+<!-- <div id="apDiv9">
+</br>
+<h3>Men&uacute; Sistema de Administraci&oacute;n de Tesis</h3>
+<div id="accordion">
+	<dl class="accordion" id="slider">
+		<dt>Administrar</dt>
+		<dd>
+		<div class="cont">
+		</br>
+			<a href="http://taller.localhost/administracion.php/AdministrarTesis">Tesis</a><br /><br />
+            <a href="http://taller.localhost/administracion.php/AdministrarAlumnos">Alumnos</a><br /><br />
+            <a href="http://taller.localhost/administracion.php/AdministrarProfesor">Profesor</a><br /><br />
+			<a href="http://taller.localhost/administracion.php/AdministrarControlReunion">Registro de Reuniones</a><br /><br />
+		</div>
+		</dd>
+		<dt>Consultar</dt>
+		<dd>
+		<div class="cont">
+		</br>
+			<a href="http://taller.localhost/consultar.php/ConsultarTesis">Tesis</a><br /><br />
+            <a href="http://taller.localhost/consultar.php/ConsultarAlumnos">Alumnos</a><br /><br />
+            <a href="http://taller.localhost/consultar.php/ConsultarProfesor">Profesor</a><br /><br />
+			<a href="http://taller.localhost/consultar.php/ConsultarControlReunion">Registro de Reuniones</a><br /><br />
+		</div>
+		</dd>
+		<dt>Reportes</dt>
+		<dd>
+		<div class="cont">
+		</br>
+			<a href="http://taller.localhost/consultar.php/ConsultarTesis/pdf">Tesis</a><br /><br />
+            <a href="http://taller.localhost/consultar.php/ConsultarAlumnos/pdf">Alumnos</a><br /><br />
+            <a href="http://taller.localhost/consultar.php/ConsultarProfesor/pdf">Profesor</a><br /><br />
+			<a href="http://taller.localhost/consultar.php/ConsultarControlReunion/pdf">Registro de Reuniones</a><br /><br />
+		</div>
+		</dd>
+		<dt>Cerrar Sesi&oacute;n</dt>
+		<dd>
+		<div class="cont">
+			</br>
+			<?php echo link_to('Cerrar Sesion', '@sf_guard_signout') ?><br /><br />
+		</div>
+		</dd>
+	</dl>
+</div>
+</div>
+<script type="text/javascript">
+var slider1=new accordion.slider("slider1");
+slider1.init("slider");
+</script>-->
+
+
+
+
