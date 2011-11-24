50fab3502f4cb51f422af1b240d306bc1028bbd7
conates <conates@conates-PC.(none)>
2011-11-23 22:55:05 -0300

    diseno nuevo

--- apps/backend/templates/_menu.php
+++ apps/backend/templates/_menu.php
@@ -1,14 +1,33 @@
-<ul>
-    <li> <?php echo link_to('Empleados', '@empleado') ?></li>
-    <li> <?php echo link_to('Usuario', '@sf_guard_user') ?></li>
-    <li> <?php echo link_to('Equipo', '@equipo') ?></li>
-    <li> <?php echo link_to('Venta de Equipos', '@ventaequipo') ?></li>
-    <li> <?php echo link_to('Categorias', '@categoria') ?></li>
-    <li> <?php echo link_to('Piezas', '@pieza') ?></li>
-    <li> <?php echo link_to('Venta de Piezas', '@ventapieza') ?></li>
-    <li> <?php echo link_to('Venta de Chatarra', '@ventachatarra') ?></li>
-    <li> <?php echo link_to('Container', '@container') ?></li>
-    <li> <?php echo link_to('Cliente', '@cliente') ?></li>
-    <li> <?php echo link_to('Salir', '@sf_guard_signout') ?></li>
+<ul id="jsddm">
+    <li><a href="#">Usuarios</a>
+        <ul>
+            <li><?php echo link_to('Empleados', '@empleado') ?></li>
+            <li><?php echo link_to('Usuario', '@sf_guard_user') ?></li>
+            <li><?php echo link_to('Cliente', '@cliente') ?></li>
+        </ul>
+    </li>
+    <li><a href="#">Ventas</a>
+        <ul>
+            <li><?php echo link_to('Venta de Equipos', '@ventaequipo') ?></li>
+            <li><?php echo link_to('Venta de Piezas', '@ventapieza') ?></li>
+            <li><?php echo link_to('Venta de Chatarra', '@ventachatarra') ?></li>
+        </ul>
+    </li>
+    <li><?php echo link_to('Piezas', '@pieza') ?></li>
+    <li><?php echo link_to('Container', '@container') ?></li>
+    <li><?php echo link_to('Equipo', '@equipo') ?></li>
+    <li><?php echo link_to('Categorias', '@categoria') ?></li>
+    <li><?php echo link_to('Salir', '@sf_guard_signout') ?></li>
 </ul>
+
+
+
+
+
+
+
+
+
+
+
 
--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -8,17 +8,44 @@
         <?php include_javascripts() ?>
         <?php include_stylesheets() ?>
         <?php use_stylesheet('admin.css') ?>
+        <script type="text/javascript">
+            var timeout         = 500;
+            var closetimer		= 0;
+            var ddmenuitem      = 0;
+
+            function jsddm_open()
+            {	jsddm_canceltimer();
+                jsddm_close();
+                ddmenuitem = $(this).find('ul').eq(0).css('visibility', 'visible');}
+
+            function jsddm_close()
+            {	if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');}
+
+            function jsddm_timer()
+            {	closetimer = window.setTimeout(jsddm_close, timeout);}
+
+            function jsddm_canceltimer()
+            {	if(closetimer)
+                {	window.clearTimeout(closetimer);
+                    closetimer = null;}}
+
+            $(document).ready(function()
+            {	$('#jsddm > li').bind('mouseover', jsddm_open);
+                $('#jsddm > li').bind('mouseout',  jsddm_timer);});
+
+            document.onclick = jsddm_close;
+        </script>
     </head>
     <body>
-        <div id="container">
-            <div id="header">
+        <div id="header">
 
-                <div class="content">
-                    <h1><a href="<?php echo url_for('homepage') ?>">
-                            <img src="/images/logo.jpg" alt="Jobeet Job Board" />
-                        </a></h1>
-                </div>
+            <div class="content">
+                <h1><a href="<?php echo url_for('homepage') ?>">
+                        <img src="/images/logo.jpg" alt="Jobeet Job Board" />
+                    </a></h1>
             </div>
+        </div>
+        <div id="container">
             <?php if ($sf_user->isAuthenticated()) { ?>
                 <div id="menu"><?php echo include_partial('global/menu'); ?></div>
             <?php } ?>
@@ -27,8 +54,7 @@
             <div id="content">
                 <?php echo $sf_content ?>
             </div>
-
-            <div id="footer"></div>
         </div>
+        <div id="footer"></div>
     </body>
 </html>
