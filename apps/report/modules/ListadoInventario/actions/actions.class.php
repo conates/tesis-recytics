192864101457e8cf84d28062db230c9f6445b25c
carolina acevedo <lcaj85@gmail.com>
2011-11-30 23:40:51 -0300

    excel

--- apps/report/modules/ListadoInventario/actions/actions.class.php
+++ apps/report/modules/ListadoInventario/actions/actions.class.php
@@ -13,4 +13,30 @@ require_once dirname(__FILE__).'/../lib/ListadoInventarioGeneratorHelper.class.p
  */
 class ListadoInventarioActions extends autoListadoInventarioActions
 {
+  public function executeIndex(sfWebRequest $request)
+  {
+    // sorting
+    if ($request->getParameter('sort'))
+    {
+      $this->setSort(array($request->getParameter('sort'), $request->getParameter('sort_type')));
+    }
+
+    // pager
+    if ($request->getParameter('page'))
+    {
+      $this->setPage($request->getParameter('page'));
+    }
+
+    $this->pager = $this->getPager();
+    $this->sort = $this->getSort();
+
+    // has filters? (usefull for activate reset button)
+    $this->hasFilters = $this->getUser()->getAttribute('ListadoInventario.filters', $this->configuration->getFilterDefaults(), 'admin_module');
+  }
+    public function executeExcel(sfWebRequest $request)
+  {
+	$this->setLayout(false);
+	$this->getResponse()->setHttpHeader('Content-type', 'application/vnd.ms-excel');
+	$this->getResponse()->setHttpHeader('Content-Disposition', 'inline; filename="Purchase_order_.xls"');
+   }
 }
