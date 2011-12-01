f095e7a3e154c4a5bfbbdc18c7d89cf7bbda7fa9
carolina acevedo <lcaj85@gmail.com>
2011-11-30 22:00:23 -0300

    modulo de inventario de productos

---
+++ apps/report/modules/ListadoInvetario/lib/ListadoInvetarioGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * ListadoInvetario module configuration.
+ *
+ * @package    tesis
+ * @subpackage ListadoInvetario
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ListadoInvetarioGeneratorConfiguration extends BaseListadoInvetarioGeneratorConfiguration
+{
+}
