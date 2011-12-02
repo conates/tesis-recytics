3e72ed4b9cc7cce8d75d1927a4ba1a158cec66fe
carolina acevedo <lcaj85@gmail.com>
2011-12-01 21:53:11 -0300

    cons cliente

---
+++ apps/consultar/modules/consultarVentaEquipo/lib/consultarVentaEquipoGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * consultarVentaEquipo module configuration.
+ *
+ * @package    tesis
+ * @subpackage consultarVentaEquipo
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class consultarVentaEquipoGeneratorConfiguration extends BaseConsultarVentaEquipoGeneratorConfiguration
+{
+}
---
+++ apps/consultar/modules/consultarVentaEquipo/lib/consultarVentaEquipoGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * consultarVentaEquipo module helper.
+ *
+ * @package    tesis
+ * @subpackage consultarVentaEquipo
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class consultarVentaEquipoGeneratorHelper extends BaseConsultarVentaEquipoGeneratorHelper
+{
+}
