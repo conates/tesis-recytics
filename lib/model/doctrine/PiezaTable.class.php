520c6a6c7dcd3c68ed8d20fe5f4e382a77458f14
conates <conates@conates-PC.(none)>
2011-12-06 23:09:12 -0300

    reportes

--- lib/model/doctrine/PiezaTable.class.php
+++ lib/model/doctrine/PiezaTable.class.php
@@ -16,4 +16,16 @@ class PiezaTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('Pieza');
     }
+    public function getLitado(){
+        $query = Doctrine_Query::create()
+                ->addSelect("r.id, r.marca as marca,r.detalle as detalle, c.tipo as tipo, c.nombre_categoria as categoria, count(r.categoria_id) as cantidad")
+                ->from("Pieza r")
+                ->innerJoin("r.Categoria c")
+                ->innerjoin("r.Usuario u")
+                ->addWhere('r.venta_pieza_id is null')
+                ->groupBy("r.marca")
+                ->fetchArray()
+        ;
+                return $query;
+    }
 }
