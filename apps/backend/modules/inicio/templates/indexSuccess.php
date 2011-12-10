23e94f5c207fd86646b8c2abaf2ab68d81f4581f
carolina acevedo <lcaj85@gmail.com>
2011-12-10 17:00:38 -0300

    dashboard

--- apps/backend/modules/inicio/templates/indexSuccess.php
+++ apps/backend/modules/inicio/templates/indexSuccess.php
@@ -3,7 +3,7 @@
         <span>Usuarios</span>
         <ul>
             <li><a href="/AdministrarEmpleados">Empleados</a></li>
-            <li><a href="/guard/users">Usuario</a></li>
+            <!--<li><a href="/guard/users">Usuario</a></li>-->
             <li><a href="/AdministrarUsuarios">Donante</a></li>
             <li><a href="/AdministrarClientes">Cliente</a></li>
         </ul>
@@ -13,7 +13,7 @@
         <ul>
             <li><a href="/AdministrarVentaEquipo">Venta de Equipos</a></li>
             <li><a href="/AdministrarVentaPieza">Venta de Piezas</a></li>
-            <li><a href="/AdministrarVentaChatarra">Venta de Chatarra</a></li>
+            <li><a href="/AdministrarVentaChatarra">Venta Chatarra</a></li>
         </ul>
     </div>
     <div class="dash">
@@ -29,7 +29,7 @@
         </ul>
     </div>
     <div class="dash">
-        <span>Container</span>
+        <span>Equipos</span>
         <ul>
             <li><a href="/AdministrarEquipos">Equipo</a></li>
         </ul>
@@ -38,6 +38,26 @@
         <span>Categorias</span>
         <ul>
             <li><a href="/AdministrarCategorias">Categorias</a></li>
+        </ul>
+    </div>
+	<div class="dash">
+        <span>Reportes</span>
+        <ul>
+            <li><a href="/AdministrarCategorias">Equipos</a></li>
+			<li><a href="/AdministrarCategorias">Piezas</a></li>
+			<li><a href="/AdministrarCategorias">Equipos y Piezas por categoria</a></li>
+			<li><a href="/AdministrarCategorias">Ventas por periodo</a></li>
+			<li><a href="/AdministrarCategorias">Ventas a recicladoras</a></li>
+        </ul>
+    </div>
+	<div class="dash">
+        <span>Consultas</span>
+        <ul>
+            <li><a href="/AdministrarCategorias">Empleados</a></li>
+			<li><a href="/AdministrarCategorias">Clientes</a></li>
+			<li><a href="/AdministrarCategorias">Donantes</a></li>
+			<li><a href="/AdministrarCategorias">Venta de equipos</a></li>
+			<li><a href="/AdministrarCategorias">Venta de piezas</a></li>
         </ul>
     </div>
     <div class="dash">
