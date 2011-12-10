957127d66949483a8e1420aa0c67f37ea65b2af6
carolina acevedo <lcaj85@gmail.com>
2011-12-10 19:07:05 -0300

    menu del reporte y consultar

--- apps/report/templates/_menu.php
+++ apps/report/templates/_menu.php
@@ -4,14 +4,14 @@
     <li><a href="#">Usuarios</a>
         <ul>
             <li><a href="/AdministrarEmpleados">Empleados</a></li>
-            <li><a href="/guard/users">Usuario</a></li>
+            <!--<li><a href="/guard/users">Usuario</a></li>-->
             <li><a href="/AdministrarUsuarios">Donante</a></li>
             <li><a href="/AdministrarClientes">Cliente</a></li>
         </ul>
     </li>
     <li><a href="#">Ventas</a>
         <ul>
-            <li><a href="/AdministrarVentaEquipo">Venta de Equipos </a></li>
+            <li><a href="/AdministrarVentaEquipo">Venta de Equipos</a></li>
             <li><a href="/AdministrarVentaPieza">Venta de Piezas</a></li>
             <li><a href="/AdministrarVentaChatarra">Venta de Chatarra</a></li>
         </ul>
@@ -22,7 +22,11 @@
     <li><a href="/AdministrarCategorias">Categorias</a></li>
     <li><a href="#">Reportes</a>
         <ul>
-            <li><a href="/report.php/ListadoInventario">Inventariado de Piezas y Equipos</a></li>
+            <li><a href="/report.php/ListadoInventario">Piezas</a></li>
+            <li><a href="/report.php/InventarioEquipo">Equipos</a></li>
+            <li><a href="/report.php/InventarioEquipo">Equipos y Piezas por categoria</a></li>
+            <li><a href="/report.php/InventarioEquipo">Ventas por periodo</a></li>
+            <li><a href="/report.php/InventarioEquipo">Ventas a recicladoras</a></li>
         </ul>
     </li>
 	<li><a href="#">Consultas</a>
