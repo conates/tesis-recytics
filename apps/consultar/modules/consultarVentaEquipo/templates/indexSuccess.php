df92940b51da6545e0d0f1ea10ef5f7752a1e158
carolina acevedo <lcaj85@gmail.com>
2011-12-11 19:47:50 -0300

    sacando el action a consultar

---
+++ apps/consultar/modules/consultarVentaEquipo/templates/indexSuccess.php
@@ -0,0 +1,32 @@
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial('consultarVentaEquipo/assets') ?>
+
+<div id="sf_admin_container">
+  <?php include_partial('consultarVentaEquipo/flashes') ?>
+
+  <div id="sf_admin_header">
+    <?php include_partial('consultarVentaEquipo/list_header', array('pager' => $pager)) ?>
+  </div>
+
+      <div id="sf_admin_bar ui-helper-hidden" style="display:none">
+      <?php include_partial('consultarVentaEquipo/filters', array('form' => $filters, 'configuration' => $configuration)) ?>
+    </div>
+  
+  <div id="sf_admin_content">
+    
+     
+
+      <?php include_partial('consultarVentaEquipo/list', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper, 'hasFilters' => $hasFilters)) ?>
+
+      <ul class="sf_admin_actions">
+        <?php include_partial('consultarVentaEquipo/list_batch_actions', array('helper' => $helper)) ?>
+      </ul>
+
+      </div>
+
+  <div id="sf_admin_footer">
+    <?php include_partial('consultarVentaEquipo/list_footer', array('pager' => $pager)) ?>
+  </div>
+
+  <?php include_partial('consultarVentaEquipo/themeswitcher') ?>
+</div>
