c1c666843d327f54e7404063ce695a53ad97df80
carolina acevedo <lcaj85@gmail.com>
2011-12-11 19:34:48 -0300

    sacando el action a reportes

---
+++ apps/report/modules/ListadoVentaPiezaBuena/templates/indexSuccess.php
@@ -0,0 +1,34 @@
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial('ListadoVentaPiezaBuena/assets') ?>
+
+<div id="sf_admin_container">
+  <?php include_partial('ListadoVentaPiezaBuena/flashes') ?>
+
+  <div id="sf_admin_header">
+    <?php include_partial('ListadoVentaPiezaBuena/list_header', array('pager' => $pager)) ?>
+  </div>
+
+      <div id="sf_admin_bar ui-helper-hidden" style="display:none">
+      <?php include_partial('ListadoVentaPiezaBuena/filters', array('form' => $filters, 'configuration' => $configuration)) ?>
+    </div>
+  
+  <div id="sf_admin_content">
+          <form action="<?php echo url_for('venta_pieza_collection', array('action' => 'batch')) ?>" method="post" id="sf_admin_content_form">
+    
+      
+
+      <?php include_partial('ListadoVentaPiezaBuena/list', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper, 'hasFilters' => $hasFilters)) ?>
+
+      <ul class="sf_admin_actions">
+        <?php include_partial('ListadoVentaPiezaBuena/list_batch_actions', array('helper' => $helper)) ?>
+      </ul>
+
+          </form>
+      </div>
+
+  <div id="sf_admin_footer">
+    <?php include_partial('ListadoVentaPiezaBuena/list_footer', array('pager' => $pager)) ?>
+  </div>
+
+  <?php include_partial('ListadoVentaPiezaBuena/themeswitcher') ?>
+</div>
